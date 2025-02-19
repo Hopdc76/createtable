181c2f30118009862643c8acce38f6adfd646765
Hopdc76 <168882726+Hopdc76@users.noreply.github.com>
2025-02-19 16:05:42 +0700

    Update createtable2.py

--- createtable2.py
+++ createtable2.py
@@ -27,6 +27,8 @@ def is_date_format(value):
             r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$'  # yyyy-mm-dd hh:mm:ss
             r'^\d{2}/\d{2}/\d{2}$',          # dd/mm/yy
             r'^\d{2}-\d{2}-\d{2}$'           # dd-mm-yy
+            r'^\d{4}/\d{2}/\d{2}$',          # yyyy/mm/dd
+            r'^\d{4}-\d{2}-\d{2}$'           # yyyy-mm-dd
         ]
         for pattern in date_patterns:
             if re.match(pattern, value):
